deba43bf6ff1c1c9cdf1a57d521985e8c2b9668a
Zohaib Tariq <zohaib.tariq@arpatech.com>
2019-09-18 14:46:40 +0500

    section 18 done please start from section 19

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
-import React            from "react"
-import ReactDom         from "react-dom"
-import {Provider}       from "react-redux"
-import {createStore}    from "redux"
-import App              from "./components/App"
-import reducers         from "./reducers"
-const store = createStore(reducers)
+import React                                    from "react"
+import ReactDom                                 from "react-dom"
+import {Provider}                               from "react-redux"
+import {createStore, applyMiddleware, compose}  from "redux"
+import App                                      from "./components/App"
+import reducers                                 from "./reducers"
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducers, composeEnhancers(applyMiddleware()))
 ReactDom.render(
     <Provider store={store}>
         <App/>
